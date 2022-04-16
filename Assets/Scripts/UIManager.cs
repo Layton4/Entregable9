0db4516def5466b65e30b1da9f3680d238b0fbe5
Layton4 <Javierete4774@gmail.com>
2022-04-16 20:19:41 +0200

    prueba_fotos

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Sprite[] Characters;
+    public Image CharacterSelection;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    /*public void Character(int selection)
+    {
+        for (int i = 0; i < Characters.Length; i++)
+        {
+            if (i == selection)
+            {
+               
+            }
+        }
+    }
+    */
+}
